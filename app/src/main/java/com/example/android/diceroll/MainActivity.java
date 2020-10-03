b7c245b56011e607d41039249c911df821588b92
BermetBatieva <bermetbatieva@gmail.com>
2020-10-04 01:09:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android/diceroll/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.diceroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.util.Random;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final Random RANDOM = new Random();
+    private Button rollDices;
+    private ImageView imageView1, imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rollDices = (Button) findViewById(R.id.rollDices);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+        rollDices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value1 = randomDiceValue();
+                int value2 = randomDiceValue();
+
+                int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.diceroll");
+                int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.diceroll");
+
+                imageView1.setImageResource(res1);
+                imageView2.setImageResource(res2);
+            }
+        });
+    }
+
+    public static int randomDiceValue() {
+        return RANDOM.nextInt(6) + 1;
+    }
+}
